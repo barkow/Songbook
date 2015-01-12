d297025a92327107b6c349a125103b28fc94e9c6
Axel Barkow <axel@euve33770.vserver.de>
2015-01-12 11:51:57 +0100

    Initialer Commit

---
+++ makeSongs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+SONGS=./songs/*.tex
+rm *.pdf
+for song in $SONGS
+do
+	echo "Erstelle $song"
+	texfile=$(basename $song .tex)
+	echo \\input{$song} > singlesonginput.tex
+	/usr/local/texlive/2014/bin/x86_64-linux/pdflatex --jobname=$texfile singlesong.tex
+done
+/usr/local/texlive/2014/bin/x86_64-linux/pdflatex songbook.tex
+/usr/local/texlive/2014/bin/x86_64-linux/pdflatex slides.tex
+mv *.pdf pdf/
+rm *.log
+rm *.aux
+
