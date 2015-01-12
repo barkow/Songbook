580549435d5df3ca52da856e2f1bf7a86b7ab7de
Axel Barkow <axel@euve33770.vserver.de>
2015-01-12 14:42:16 +0100

    textbook als weitere Variante hinzugefügt

--- makeSongs.sh
+++ makeSongs.sh
@@ -9,6 +9,7 @@ do
 	/usr/local/texlive/2014/bin/x86_64-linux/pdflatex --jobname=$texfile singlesong.tex
 done
 /usr/local/texlive/2014/bin/x86_64-linux/pdflatex songbook.tex
+/usr/local/texlive/2014/bin/x86_64-linux/pdflatex textbook.tex
 /usr/local/texlive/2014/bin/x86_64-linux/pdflatex slides.tex
 mv *.pdf pdf/
 rm *.log
