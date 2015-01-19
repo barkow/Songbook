66613c3b9c2b46929cfea9f03f7eb7508f892ade
Axel Barkow <axel@euve33770.vserver.de>
2015-01-19 12:13:52 +0100

    Erzeugen der einzelnen Dokumente überarbeitet

--- makeSongs.sh
+++ makeSongs.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 SONGS=./songs/*.tex
 rm *.pdf
+echo \\begin{songs}{} > songlist.tex
 for song in $SONGS
 do
 	echo "Erstelle $song"
 	texfile=$(basename $song .tex)
 	echo \\input{$song} > singlesonginput.tex
+	echo \\include{songs/$(basename $song .tex)} >> songlist.tex
+	echo \\ifchorded >> songlist.tex
+	echo \\sclearpage >> songlist.tex
+	echo \\fi >> songlist.tex
+
 	/usr/local/texlive/2014/bin/x86_64-linux/pdflatex --jobname=$texfile singlesong.tex
 done
+echo \\end{songs} >> songlist.tex
 /usr/local/texlive/2014/bin/x86_64-linux/pdflatex songbook.tex
 /usr/local/texlive/2014/bin/x86_64-linux/pdflatex textbook.tex
 /usr/local/texlive/2014/bin/x86_64-linux/pdflatex slides.tex
